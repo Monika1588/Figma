b3e6e5a96741fb2346d9e1ea69132327aee0d10b
Monika 1588 <vermamonika3733@gmail.com>
2026-02-13 09:44:09 +0530

    commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,9 +8,7 @@ const Footer = () => {
         <p className="footer-text"><b>Follow Us For More Information</b></p>
         <div className="socials">
           <a href="#"><img src="/icons/fb.svg" alt="Facebook" /></a>
-          <a href="#"><img src="/icons/fb.svg" alt="LinkedIn" /></a>
           <a href="#"><img src="/icons/yt.svg" alt="YouTube" /></a>
-          <a href="#"><img src="/icons/p.svg" alt="Instagram" /></a>
           <a href="#"><img src="/icons/p.svg" alt="Pinterest" /></a>
         </div>
       </div>
